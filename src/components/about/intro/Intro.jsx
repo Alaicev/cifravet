26ddd8a5fbc60f556f7eb2f69442806b56f02987
Alaicev <altarev123456@mail.ru>
2024-10-12 00:38:09 +0400

    first

--- src/components/about/intro/Intro.jsx
+++ src/components/about/intro/Intro.jsx
@@ -8,10 +8,10 @@ function About(props) {
       <div className="wrapper">
         <div className="about__container">
           <h1 className="about__title">
-          ВетЦифра - <br />
-          <span className='about__sub-title'>это современная цифровая образовательная платформа, которая объединяет курсы 
-          по различным аспектам ветеринарной работы.</span>
-          </h1>
+          ВетЦифра - </h1>
+          <p className='about__sub-title'>это современная цифровая образовательная платформа, которая объединяет курсы 
+          по различным аспектам ветеринарной работы.
+          </p>
           <NavLink to="course" className="button about__button">Смотреть курсы</NavLink>
         </div>
       </div>
